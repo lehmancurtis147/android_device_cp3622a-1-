34bf82cc6864d77617b72d56416003932a83f75f
lehmancurtis147 <lehmancurtis147@gmail.com>
2018-03-26 07:09:46 -0500

    Yet to be finished make files.

--- Android.mk
+++ Android.mk
@@ -1,14 +1,18 @@
 #
 # This empty Android.mk file exists to prevent the build system from
 # automatically including any other Android.mk files under this directory.
-#
+#LOCAL_PATH variable
+ LOCAL_PATH := $(device/Coolpad/REL)
+ 
+ include $(CLEAR_VARS)
+ 
+ include $(BUILD_SHARED_LIBRARY)
 
-#include $(CLEAR_VARS)
-#LOCAL_MODULE       := wpa_supplicant.conf
+ LOCAL_MODULE       := wpa_supplicant.conf
 #LOCAL_MODULE_TAGS  := optional
 #LOCAL_MODULE_CLASS := ETC
 #LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 #LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
-#include $(BUILD_PREBUILT)
+ include $(BUILD_PREBUILT)
 
-#include $(call all-makefiles-under,$(LOCAL_PATH))
+ include $(call all-makefiles-under,$(LOCAL_PATH))
