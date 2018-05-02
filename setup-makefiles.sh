a67ec069e8c2910874c6206f43ae8773645fa5c9
lehmancurtis147 <36573984+lehmancurtis147@users.noreply.github.com>
2018-05-01 22:27:59 -0500

    Update setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -41,7 +41,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" "true"
 
 # Copyright headers and guards
-write_headers "scale"
+write_headers "cp3622a"
 
 # The standard common blobs
 write_makefiles "$MY_DIR"/proprietary-files.txt
