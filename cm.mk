db708b750e9d9e47631804554129e107e8118d62
Curtis Lehman <36573984+lehmancurtis147@users.noreply.github.com>
2018-11-23 20:03:59 -0600

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
 # Release name
-PRODUCT_RELEASE_NAME := REL
+PRODUCT_RELEASE_NAME := cp3622a
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/Coolpad/REL/device_REL.mk)
+$(call inherit-product, device/coolpad/cp3622a/device_cp3622a.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := REL
-PRODUCT_NAME := cm_REL
+PRODUCT_DEVICE := cp3622a
+PRODUCT_NAME := cm_cp3622a
 PRODUCT_BRAND := Coolpad
-PRODUCT_MODEL := REL
+PRODUCT_MODEL := cp3622a
 PRODUCT_MANUFACTURER := Coolpad
